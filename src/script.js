39c1a236d99e1c308c95f4c099619ec12ffe3a63
Bill Zay <76415515+billzayy@users.noreply.github.com>
2022-11-23 16:24:37 +0700

    Change Background

--- src/script.js
+++ src/script.js
@@ -1,5 +1,23 @@
-const subTilte = document.querySelector('.sub-title');
+const subTitle = document.querySelector('.sub-title');
+const backgroundColor = document.querySelector('body');
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
 
 setTimeout(() => {
-    subTilte.innerHTML = "Hello Etoners! "
+    subTitle.innerHTML = "Hello Etoners! "
 }, 2000);
+
+
+setInterval(() => {
+    let hexColor = "#"
+
+    for (let i = 0; i < 6; i++) {
+        hexColor += arr[Math.floor(Math.random() * arr.length)]
+    }
+
+    // colorValue.innerHTML = hexColor
+    console.log(hexColor);
+    backgroundColor.style.backgroundColor = hexColor
+}, 1000)
+
+// changeColor()
